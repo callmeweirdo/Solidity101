3b18d1c7b0af43627be8971d57364350b701bd90
calleweirdo <ogbandav102@gmail.com>
2023-12-08 07:23:51 +0100

    done with this lesson and entering another one

--- index.sol
+++ index.sol
@@ -9,17 +9,32 @@ pragma solidity >=0.8.2 <0.9.0;
  */
 
 contract SimpleContract {
+
+    uint256 public publicNumber;
+
     struct Person {
         string name;
         uint256 number;
+    }
+
+    function store(uint256 _publicNumber) public {
+        publicNumber = _publicNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return publicNumber;
     }
 
     // Person public David = Person({name: "OG", number: 101});
 
     Person[] public people;
 
+    mapping(string => uint256) public personNumber;
+
     function addPerson(string memory _name, uint256 _number) public {
         Person memory newPerson = Person(_name, _number);
         people.push(newPerson);
+        personNumber[_name] = _number;
+
     }
 }
