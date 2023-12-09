9744bc7108f78001be6cce10abba3f4138bd704a
calleweirdo <ogbandav102@gmail.com>
2023-12-09 10:46:08 +0100

    created a new contract in the SimpleFactory file and ran it to see how to deploy a contract from anothr contract

---
+++ StorageFactory.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "./index.sol";
+
+contract StorageFactory{
+    // uint256 public name;
+
+    SimpleContract public simpleContract;
+
+    function createSimpleStorageContract() public {
+        simpleContract = new SimpleContract();
+    }  
+}
