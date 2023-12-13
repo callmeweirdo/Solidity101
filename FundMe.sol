f4d744744433fef8cdf9e43d898ca14825bb25c6
calleweirdo <ogbandav102@gmail.com>
2023-12-13 15:00:58 +0100

    I just implemented the price checker functionality based on the current price of the ETH Network

--- FundMe.sol
+++ FundMe.sol
@@ -6,12 +6,32 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
+
+    AggregatorV3Interface aggregator;
+
+    constructor(AggregatorV3Interface _aggregator){
+        aggregator = _aggregator;
+    }
+
+    uint256 public minimumUsd = 5;
+
     function fund() public payable {
         // send funds
         // allow users to send money
         // have a minimum $ to send
         // now how do we send Eth to this contract ?
         require(msg.value >= 1e18, "Please top up your balance to make this transaction"); // 1e18 = 1 ETH = 1000000000000000000
+    }
+
+    function getPrice() public view returns (uint256) {
+        // Sepolia Eth address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+          (,int256 answer, , , ) = aggregator.latestRoundData();
+         return uint(answer * 1e10);
+
+        //(, int256 answer,,,) = aggregator.latestRoundData();
+        //return uint256(answer);
     }
 }
