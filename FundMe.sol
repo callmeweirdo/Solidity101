f4f995e4dbc3c10c650216119e4e8193fe7383a1
calleweirdo <ogbandav102@gmail.com>
2023-12-15 04:14:40 +0100

    added the get current pricing feature and also the convert price from Eth to Dollar feature to the contract

--- FundMe.sol
+++ FundMe.sol
@@ -16,22 +16,29 @@ contract FundMe {
         aggregator = _aggregator;
     }
 
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5e18;
 
     function fund() public payable {
         // send funds
         // allow users to send money
         // have a minimum $ to send
         // now how do we send Eth to this contract ?
-        require(msg.value >= 1e18, "Please top up your balance to make this transaction"); // 1e18 = 1 ETH = 1000000000000000000
+        require(getPriceInUsd(msg.value) >= 1e18, "Please top up your balance to make this transaction"); // 1e18 = 1 ETH = 1000000000000000000
     }
 
     function getPrice() public view returns (uint256) {
         // Sepolia Eth address 0x694AA1769357215DE4FAC081bf1f309aDC325306
           (,int256 answer, , , ) = aggregator.latestRoundData();
-         return uint(answer * 1e10);
+         return uint(answer * 1e10); // 2277945563880000000000
 
         //(, int256 answer,,,) = aggregator.latestRoundData();
         //return uint256(answer);
+    }
+
+    function getPriceInUsd(uint256 userPrice) public view returns(uint256){
+        uint256 currentPrice = getPrice(); 
+        return (currentPrice * userPrice) / 1e10 ;
+        // 114942289698000000000000
+        //  $2304.29288149
     }
 }
