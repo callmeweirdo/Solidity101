dc6e2507cb69ee3687dc29b09fa16a84654bda1a
calleweirdo <ogbandav102@gmail.com>
2023-12-17 04:50:05 +0100

    after adding some more features like mapping through the addresses used to fund in other to get the value sent from each the address, i also created a Library to house most of the functions from the fund me contract

--- FundMe.sol
+++ FundMe.sol
@@ -8,37 +8,28 @@ pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+import {PriceConverter} from "./PriceConverter.sol";
+
 contract FundMe {
+    
+    using PriceConverter for uint256;
 
-    AggregatorV3Interface aggregator;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
-    constructor(AggregatorV3Interface _aggregator){
+    AggregatorV3Interface public aggregator;
+
+    constructor(AggregatorV3Interface _aggregator) {
         aggregator = _aggregator;
     }
 
     uint256 public minimumUsd = 5e18;
 
     function fund() public payable {
-        // send funds
-        // allow users to send money
-        // have a minimum $ to send
-        // now how do we send Eth to this contract ?
-        require(getPriceInUsd(msg.value) >= minimumUsd, "Please top up your balance to make this transaction"); // 1e18 = 1 ETH = 1000000000000000000
-    }
+    uint256 ethAmount = msg.value;
+    require(PriceConverter.getPriceInUsd(aggregator, ethAmount) >= minimumUsd, "Please top up your balance to make this transaction");
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] = ethAmount;
+}
 
-    function getPrice() public view returns (uint256) {
-        // Sepolia Eth address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-          (,int256 answer, , , ) = aggregator.latestRoundData();
-         return uint(answer * 1e10); // 2277945563880000000000
-
-        //(, int256 answer,,,) = aggregator.latestRoundData();
-        //return uint256(answer);
-    }
-
-    function getPriceInUsd(uint256 userPrice) public view returns(uint256){
-        uint256 currentPrice = getPrice(); 
-        return (currentPrice * userPrice) / 1e10 ;
-        // 114942289698000000000000
-        //  $2304.29288149
-    }
-}//5000000000000000000
+}
